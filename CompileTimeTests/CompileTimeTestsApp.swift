ecf0e33b126174d0d2c09af46336c85ff33aa321
Kapil Rathore <kapil.rathore@compass.com>
2022-09-26 13:18:15 +0530

    Initial Commit

---
+++ CompileTimeTests/CompileTimeTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompileTimeTestsApp.swift
+//  CompileTimeTests
+//
+//  Created by Kapil Rathore on 26/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CompileTimeTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
